654fbd959b102501caea74ecea7e571af7317209
Alex Rocha <alexsandro.fernandes.3@sesisenaisp.org.br>
2025-01-15 19:06:01 -0300

    criação da classe funcionario e atributos

---
+++ backend/modules/Nexus.Folha/src/Nexus.Folha.Domain/Entities/Funcionarios.cs
@@ -0,0 +1,30 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Nexus.Folha.Domain.Entities;
+
+public class Funcionarios 
+{
+    public string NomeCompleto { get; set; }
+    
+    public string DataNascimento { get; set; }
+    
+    public string NomePai { get; set; }
+    
+    public string NomeMae { get; set; }
+
+    public  string Cep { get; set; }
+    
+    public string Endereço { get;set; }                                             
+
+    public string Cidade { get; set; }
+    
+    public string UF { get; set; }
+
+    public string LocalNasimento { get; set; }
+
+    public string Telefone { get; set; }
+    
+    public string NomeConjuge { get; set; }
+
+}
+
