2eb10fa50c21efd36b005a1c3681bcd73850e2e7
Alex Rocha <alexsandro.fernandes.3@sesisenaisp.org.br>
2025-01-16 16:05:39 -0300

    Criação da classe Ferias com campos e atributos

---
+++ backend/modules/Nexus.Folha/src/Nexus.Folha.Domain/Entities/Ferias.cs
@@ -0,0 +1,10 @@
+namespace Nexus.Folha.Domain.Entities;
+
+public class Ferias
+{
+    public int Id { get; set; }
+    
+    public DateTime DataInicio { get; set; }
+    
+    public DateTime DataFim { get; set; }
+}
