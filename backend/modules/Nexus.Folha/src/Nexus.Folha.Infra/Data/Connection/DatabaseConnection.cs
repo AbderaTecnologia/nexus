3beb4a147bfba8771773806109eda78c95349c4b
Alex Rocha <alexsandro@qualyteam.com.br>
2025-01-18 16:35:29 -0400

    correções API

--- backend/modules/Nexus.Folha/src/Nexus.Folha.Infra/Data/Connection/DatabaseConnection.cs
+++ backend/modules/Nexus.Folha/src/Nexus.Folha.Infra/Data/Connection/DatabaseConnection.cs
@@ -1,6 +1,6 @@
 using System.Data;
-using System.Data.SQLite;
 using Dapper;
+using Microsoft.Data.Sqlite;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Nexus.Folha.Infra.Data.Connection;
@@ -9,7 +9,7 @@ public static class DatabaseConnections
 {
     public static IServiceCollection AddDatabase(this IServiceCollection services)
     {
-        var connection = new SQLiteConnection("Data Source=:memory:");
+        var connection = new SqliteConnection("Data Source=:memory:");
 
         return services
             .AddSingleton<IDbConnection>(_ => connection)
