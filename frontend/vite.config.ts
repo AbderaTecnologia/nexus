68faa4dc78f8a364484c5ca77851914d8c0f31ec
Alex Rocha <alexsandro@qualyteam.com.br>
2025-01-18 17:50:58 -0400

    pequenos ajustes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://localhost:7143',
         changeOrigin: true,
         secure: false
       }
