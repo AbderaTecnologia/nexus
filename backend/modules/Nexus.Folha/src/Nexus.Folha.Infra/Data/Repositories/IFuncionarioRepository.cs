169a54d170bf50411ca2081f4d132dc0919cc6fe
Alex Rocha <alexsandro.fernandes.3@sesisenaisp.org.br>
2025-01-20 15:22:37 -0300

    ajustes IFuncioanrioRepository

--- backend/modules/Nexus.Folha/src/Nexus.Folha.Infra/Data/Repositories/IFuncionarioRepository.cs
+++ backend/modules/Nexus.Folha/src/Nexus.Folha.Infra/Data/Repositories/IFuncionarioRepository.cs
@@ -1,20 +1,12 @@
-using System.Data.Entity.Infrastructure.Interception;
-using Nexus.Folha.Api;
 using Nexus.Folha.Domain.Entities;
 
-namespace Nexus.Folha.Infra.Data.Repositories
+namespace Nexus.Folha.Infra.Data.Repositories;
 
 
-{
-public interface IFuncionarioRepository
-{
-    Task<IEnumerable<Funcionario>?>Get();
-    Task<FuncionarioRespository> Get(Guid id);
-    Task<bool?> Create(Funcionario funcionario);
-    Task<bool?> Update(Funcionario funcionario);
-    Task<bool?> Delete(Guid id);
-
-}
+    public interface IFuncionarioRepository
+    {
+        Task<IEnumerable<Funcionario>?>Get();  
+    }
     
-    
-}
+
+   
