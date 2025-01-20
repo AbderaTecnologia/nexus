aab5df41cb34c0270bdf62064415c4289892c6b0
Alex Rocha <alexsandro.fernandes.3@sesisenaisp.org.br>
2025-01-20 16:37:50 -0300

    criação da classe Pro Labore

---
+++ backend/modules/Nexus.Folha/src/Nexus.Folha.Domain/Entities/ProLabore.cs
@@ -0,0 +1,10 @@
+namespace Nexus.Folha.Domain.Entities;
+
+public class ProLabore
+{
+    public Guid Id {get; set;}
+    
+    public string Declarão {get; set;}
+    
+    public DateTime Período { get; set; }
+}
