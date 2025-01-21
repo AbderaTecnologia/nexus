aee025bc1e748b9f3c0f101812b5205d4ddeebcc
Alex Rocha <alexsandro.fernandes.3@sesisenaisp.org.br>
2025-01-21 17:18:31 -0300

    ajuste no repositorio IFuncionario

--- backend/modules/Nexus.Folha/src/Nexus.Folha.Infra/Repositories/IFuncionarioRepository.cs
+++ backend/modules/Nexus.Folha/src/Nexus.Folha.Infra/Repositories/IFuncionarioRepository.cs
@@ -5,7 +5,7 @@ namespace Nexus.Folha.Infra.Repositories;
 
 public interface IFuncionarioRepository
 {
-    Task<IEnumerable<Funcionario>?> Get();
+    Task<IEnumerable<Funcionario>> Get();
     Task<Funcionario?> Get(Guid id);
     Task<bool?> Create(Funcionario funcionario);
     Task<bool?> Update(Funcionario funcionario);
