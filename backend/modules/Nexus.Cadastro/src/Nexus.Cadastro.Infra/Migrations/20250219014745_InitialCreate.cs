d04c7e066ce4e7a64be3102033c920d578736afc
Alex Rocha <alexrocha.f@outlook.com>
2025-02-18 21:54:11 -0400

    Atualiza a conexão com o banco de dados para usar "CadastroPostgresConnection", adiciona referências de projeto e pacotes, e implementa a migração inicial para a tabela "Companies".

---
+++ backend/modules/Nexus.Cadastro/src/Nexus.Cadastro.Infra/Migrations/20250219014745_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexus.Cadastro.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CompanyType = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    ContabilidadeId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Identifier = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Companies_Companies_ContabilidadeId",
+                        column: x => x.ContabilidadeId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_ContabilidadeId",
+                table: "Companies",
+                column: "ContabilidadeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
