beea7b5d308c3e94d0929bd2cf0f09b70a71641f
Alex Rocha <alexrocha.f@outlook.com>
2025-02-18 21:16:25 -0400

    Atualiza pacotes do Microsoft.IdentityModel.Tokens e System.IdentityModel.Tokens.Jwt para a versão 8.5.0, adiciona suporte a IPasswordHasher no gerenciamento de usuários e refatora a classe User para utilizar construtores e métodos de proteção de senha.

--- backend/modules/Nexus.Auth/src/Nexus.Auth.Domain/Entities/User.cs
+++ backend/modules/Nexus.Auth/src/Nexus.Auth.Domain/Entities/User.cs
@@ -1,9 +1,17 @@
 namespace Nexus.Auth.Domain.Entities;
 
-public class User
+public class User(
+    string username,
+    Guid companyId
+)
 {
     public int Id { get; init; }
-    public string Username { get; init; } = string.Empty;
-    public string Password { get; init; } = string.Empty;
-    public Guid CompanyId { get; init; }
+    public string Username { get; init; } = Guard.Against.NullOrEmpty(username, nameof(username));
+    public string? Password { get; private set; }
+    public Guid CompanyId { get; init; } = Guard.Against.Default(companyId, nameof(companyId));
+
+    public void SetHashPassword(string passwordHasher)
+    {
+        Password = Guard.Against.NullOrEmpty(passwordHasher, nameof(passwordHasher));
+    }
 }
