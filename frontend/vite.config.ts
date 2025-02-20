bd61bd8bc15b77c93201ad724c7a4b167cce7f1a
Alex Rocha <alexsandro@qualyteam.com.br>
2025-02-19 21:47:46 -0400

    Adiciona configuração do Sentry e plugin Vite para monitoramento de erros

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
@@ -5,7 +6,10 @@ import dynamicImport from 'vite-plugin-dynamic-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dynamicImport()],
+  plugins: [react(), dynamicImport(), sentryVitePlugin({
+    org: "alex-kav-rocha",
+    project: "nexus-qn1qaa"
+  })],
   assetsInclude: ['**/*.md'],
   resolve: {
     alias: {
@@ -27,6 +31,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    sourcemap: true
   }
 })
