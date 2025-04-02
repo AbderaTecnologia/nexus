8c2e6e919383dcee93af1ebe72304fcd65e6f63a
Álef Chrístian <alefchristian4125@gmail.com>
2025-04-02 17:01:47 -0300

    ajustes na classe ProfileImageDto.cs

--- backend/modules/Nexus.Cadastro/src/Nexus.Cadastro.Application/Models/Dtos/ProfileImageDto.cs
+++ backend/modules/Nexus.Cadastro/src/Nexus.Cadastro.Application/Models/Dtos/ProfileImageDto.cs
@@ -1,3 +1,21 @@
+using FluentValidation;
+
 namespace Nexus.Cadastro.Application.Models.Dtos;
 
-public class ProfileImageDto("src:");
+public sealed record ProfileImageDto(
+    string Src
+    );
+
+public sealed class ProfileImageDtoValidator : AbstractValidator<ProfileImageDto>
+{
+    public ProfileImageDtoValidator()
+    {
+        RuleFor(x => x.Src)
+            .NotEmpty().WithMessage("Src é obrigatório")
+            .MaximumLength(1).WithMessage("O campo deve conter o máximo um src");
+    }
+}
+
+
+
+
