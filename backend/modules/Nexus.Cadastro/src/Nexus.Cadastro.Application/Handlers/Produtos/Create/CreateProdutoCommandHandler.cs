172255232b7b79950ee36f688c096e03b50e256a
Álef Chrístian <alefchristian4125@gmail.com>
2025-05-16 19:17:43 -0300

    Ajustes nas classes CreateProdutoCommandHandler.cs/ CadastroDbContext.cs/ Produto.cs/ ProdutoQuery.cs

--- backend/modules/Nexus.Cadastro/src/Nexus.Cadastro.Application/Handlers/Produtos/Create/CreateProdutoCommandHandler.cs
+++ backend/modules/Nexus.Cadastro/src/Nexus.Cadastro.Application/Handlers/Produtos/Create/CreateProdutoCommandHandler.cs
@@ -1,22 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Nexus.Cadastro.Domain.Entities;
 using Nexus.Cadastro.Infra.Persistence;
+using Nexus.Core.Domain.Entities;
 
 namespace Nexus.Cadastro.Application.Handlers.Produtos.Create;
 
-public sealed class CreateProdutoCommandHandler(CadastroDbContext cadastroDbContext) : IRequestHandler<CreateProdutoCommand, IResult>
+public sealed class CreateProdutoCommandHandler
 {
-    public async Task Handler(CreateProdutoCommand request, CancellationToken cancellationToken)
+    private readonly CadastroDbContext _context;
+    public CreateProdutoCommandHandler(CadastroDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<IResult> Handler(CreateProdutoCommand request, CancellationToken cancellationToken)
     {
         var validator = new CreateProdutoCommandValidator();
         var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
         if (!validationResult.IsValid)
         {
+            var errors = validationResult.Errors
+                .GroupBy(x => x.ErrorMessage)
+                .ToDictionary(g => g
+                        .Key, g => g.Select(e => e
+                        .ErrorMessage).ToArray()
+                );
+
             var problemDetails = new ValidationProblemDetails
             {
-                
-            }
+                Title = " Falha na validação",
+                Status = StatusCodes.Status400BadRequest,
+                Detail = "um ou mais erros ocorreram na validação dos dados!",
+                Instance = "/produtos/create",
+            };
+
+            return BadRequest(problemDetails);
         }
+
+        var produto = new Produto
+        {
+            Id = request.Id,
+            Nome = request.Nome,
+            Preco = request.Preco,
+            Estoque = request.Estoque,
+            Descricao = request.Descricao
+        };
+
+        _context.Produtos.Add(produto);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Created($"/api/cadastro/produto/{produto.Id}", produto.Id);
     }
 }
+
--- backend/modules/Nexus.Cadastro/src/Nexus.Cadastro.Domain/Entities/Produto.cs
+++ backend/modules/Nexus.Cadastro/src/Nexus.Cadastro.Domain/Entities/Produto.cs
@@ -1,10 +1,13 @@
+using Nexus.Core.Domain.Entities.Base;
+
 namespace Nexus.Cadastro.Domain.Entities;
 
-public class CompanyProduto
+public sealed class Produto : EntitySoftDeletedBase
 {
     public Guid Id { get; set; }
     public string Nome { get; set; }
-    public string Descricao { get; set; }
     public double Preco { get; set; }
     public int Estoque { get; set; }
+    public string Descricao { get; set; }
+    
 }
